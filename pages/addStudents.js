c73b315cc3d2c1cba8d2bc349e8b3236e932d59d
Rahim Virani <rkvirani@outlook.com>
2022-03-08 10:18:26 -0800

    Added addPage that will add to htmlLocalStorage

---
+++ pages/addStudents.js
@@ -0,0 +1,63 @@
+import FlexWrapper from '../components/FlexWrapper'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import Main from '../components/Main'
+import Nav from '../components/Nav'
+
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+
+
+const handleSubmit = (e) => {
+  //Pull all the data from the form
+  let firstName = document.getElementById("firstName").value;
+  let lastName = document.getElementById("lastName").value;
+  let program = document.getElementById("program").value;
+  //Take the data and create a student object
+  const student = {
+    firstName: firstName,
+    lastName: lastName,
+    program: program
+  }
+
+  //Store the student object in the html5storage
+  if (localStorage.getItem('students')) {
+    //The key exists
+    var currentStudents = JSON.parse(localStorage.getItem('students'));
+  } else {
+    //If nothing exists make an empty array
+    var currentStudents = [];
+  }
+
+  //Add the student
+  currentStudents.push(student);
+  //Store the student
+  localStorage.setItem('students',JSON.stringify(currentStudents));
+
+}
+export default function ListStudentsPage() {
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+      </Head>
+      <FlexWrapper>
+        <Header>Sam Hill's Discount Student App</Header>
+        <Nav></Nav>
+        <Main>
+            <table>
+                 <tr><td>firstName</td><td><input type="text" id="firstName" value="Sam"/></td></tr>
+                 <tr><td>lastName</td><input type="text" id="lastName" value="Hill"/></tr>
+                 <tr><td>Program</td><td><select id="program">
+                   <option value="CSTP">Computer Systems Technology Program</option>
+                   <option value="ERT">Electronics Repair Technician</option>
+                   <option value="FASH">Fashion Design</option></select></td></tr>
+                 <tr><td colspan="2"><button onClick={(e) => {handleSubmit()}}>Add Student</button></td></tr>
+                 </table></Main>
+      <Footer></Footer>
+    </FlexWrapper>
+    </>
+  )
+}
